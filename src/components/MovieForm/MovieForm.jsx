f5fc1a2b064615ca0fac855f84859b426e423018
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-18 10:08:34 -0500

    added dropdown menu to add movies page

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,31 +1,47 @@
-import FormControl from '@mui/material/FormControl';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { useState } from "react";
 
 function MovieForm() {
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const genreList = ["Adventure"];//useSelector(store => store.movies);
 
-
-  return(
+  return (
     <div>
-
-<FormControl variant="standard">
-    <InputLabel htmlFor="component-simple">Movie Name</InputLabel>
-    <Input id="component-simple" />
-  </FormControl>
-  <FormControl variant="standard">
-    <InputLabel htmlFor="component-simple">Poster URL</InputLabel>
-    <Input id="component-simple" />
-  </FormControl>
-  <br></br>
-  <FormControl variant="standard">
-    <InputLabel htmlFor="component-simple">Description</InputLabel>
-    <OutlinedInput id="component-simple" />
-  </FormControl>
-  </div>
-    
-  )
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-simple">Movie Name</InputLabel>
+        <Input id="component-simple" />
+      </FormControl>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-simple">Poster URL</InputLabel>
+        <Input id="component-simple" />
+      </FormControl>
+      <br></br>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-simple">Description</InputLabel>
+        <OutlinedInput id="component-simple" />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: 500 }}>
+      <InputLabel>Multiple Select</InputLabel>
+      <Select
+        multiple
+        value={selectedGenres}
+        onChange={(e) => setSelectedGenres(e.target.value)}
+        input={<OutlinedInput label="Multiple Select" />}
+      >
+        {genreList.map((genre) => (
+          <MenuItem key={genre} value={genre}>
+            {genre}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+    </div>
+  );
 }
 
 export default MovieForm;
