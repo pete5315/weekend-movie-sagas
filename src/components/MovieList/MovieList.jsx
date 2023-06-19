6470b4b569fdb887a2c668ba58b8c3709ee10859
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-18 23:16:49 -0500

    added add a movie button to the list page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -8,10 +8,15 @@ import MovieListItem from "../MovieListItem/MovieListItem";
 // MUI components used on this page
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom/";
 
 function MovieList() {
   // enable dispatch
   const dispatch = useDispatch();
+  // enable history
+  const history = useHistory();
   // access the movies part of the store
   const movies = useSelector((store) => store.movies);
 
@@ -21,8 +26,16 @@ function MovieList() {
     dispatch({ type: "FETCH_MOVIES" });
   }, []);
 
+  function goToMovieForm() {
+    console.log("YES");
+    ;
+  }
+
   return (
     <main>
+      <Button variant="contained" onClick={() => history.push("/movieform")}>
+        Add a movie!
+      </Button>
       <Box sx={{ flexGrow: 1 }}>
         {/* Grid enables relatively equivalent spacing */}
         <Grid container spacing={2}>
